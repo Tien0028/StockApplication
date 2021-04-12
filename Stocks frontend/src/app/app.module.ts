7204f6a25b4a871ee9293a7b55a76a98c1098cb9
Tien0028 <56824916+Tien0028@users.noreply.github.com>
2021-04-12 09:10:38 +0200

    NGXS installation

--- Stocks frontend/src/app/app.module.ts
+++ Stocks frontend/src/app/app.module.ts
@@ -1,14 +1,30 @@
-import { NgModule } from '@angular/core';
+import {Injectable, NgModule} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {SocketIoConfig, SocketIoModule} from 'ngx-socket-io';
+import {Socket, SocketIoConfig, SocketIoModule} from 'ngx-socket-io';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {SharedModule} from './shared/shared.module';
 import {MatCardModule} from '@angular/material/card';
 import { AddStockComponent } from './stock-exchange/shared/add-stock/add-stock.component';
+import { NgxsModule } from '@ngxs/store';
+import {environment} from '../environments/environment.prod';
 
 const config: SocketIoConfig = { url: 'http://localhost:3001', options: {} };
+
+@Injectable()
+export class SocketChat extends Socket {
+  constructor() {
+    super({ url: 'http://localhost:3000', options: {} });
+  }
+}
+
+@Injectable()
+export class SocketStocks extends Socket {
+  constructor() {
+    super({ url: 'http://localhost:3001', options: {} });
+  }
+}
 
 @NgModule({
   declarations: [
@@ -22,6 +38,9 @@ const config: SocketIoConfig = { url: 'http://localhost:3001', options: {} };
     BrowserAnimationsModule,
     SharedModule,
     MatCardModule,
+    NgxsModule.forRoot([], {
+      developmentMode: !environment.production
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
